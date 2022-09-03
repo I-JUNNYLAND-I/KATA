2ac7b7923ee60c2b03bc1cc407416fd1713eadbc
Junnyjun <chbe5082@naver.com>
2022-09-03 14:28:15 +0900

    init

--- java-baseball/src/test/java/git/io/baseball/BaseBallLotteryTest.java
+++ java-baseball/src/test/java/git/io/baseball/BaseBallLotteryTest.java
@@ -3,7 +3,6 @@ package git.io.baseball;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.random.RandomGenerator;
 
@@ -12,10 +11,8 @@ class BaseBallLotteryTest {
     RandomGenerator randomGenerator = RandomGenerator.getDefault();
 
     @Test
-    void 숫자_추출하김(){
+    void 숫자_추출하김() {
         List<Integer> baseBallLottery = BaseBallLottery.extract(randomGenerator);
-
         Assertions.assertThat(baseBallLottery).hasSize(3);
-
     }
 }
