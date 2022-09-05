89b5bc2c28072a431edc95c8fb0830abd0d78a3b
Junnyjun <chbe5082@naver.com>
2022-09-05 23:26:09 +0900

    init

--- java-string/src/test/java/git/io/CalculateTest.java
+++ java-string/src/test/java/git/io/CalculateTest.java
@@ -2,6 +2,7 @@ package git.io;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
